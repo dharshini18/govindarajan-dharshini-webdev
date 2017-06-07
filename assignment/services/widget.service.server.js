07ce07f931d5153e9cd8c1be8d335a5d1c2668bd
dharshini18 <govindarajan.d@husky.neu.edu>
2017-06-06 23:32:25 -0400

    Commit7 - Image Upload

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -19,6 +19,44 @@ app.get("/api/widget/type/:widgetId",findWidgetType);
 app.put ('/api/widget/:widgetId', updateWidget);
 app.delete('/api/widget/:widgetId',deleteWidget);
 
+
+    var multer = require('multer'); // npm install multer --save
+    var upload = multer({ dest: __dirname+'/../../public/assignment/uploads' });
+
+    app.post ("/api/upload", upload.single('myFile'), uploadImage);
+
+    function uploadImage(req, res) {
+
+        var widgetId      = req.body.widgetId;
+        var width         = req.body.width;
+        var myFile        = req.file;
+
+        var userId = req.body.userId;
+        var websiteId = req.body.websiteId;
+        var pageId = req.body.pageId;
+
+        var originalname  = myFile.originalname; // file name on user's computer
+        var filename      = myFile.filename;     // new file name in upload folder
+        var path          = myFile.path;         // full path of uploaded file
+        var destination   = myFile.destination;  // folder where file is saved to
+        var size          = myFile.size;
+        var mimetype      = myFile.mimetype;
+
+        widget = getWidgetById(widgetId);
+        widget.url = '/assignment/uploads/'+filename;
+
+        var callbackUrl   = "/assignment/index.html#!/user/"+userId+"/website/"+websiteId+"/page/"+pageId+"/widget/"+widgetId;
+        res.redirect(callbackUrl);
+    }
+
+    function getWidgetById(widgetId) {
+        for(var w in widgets){
+            if(widgets[w]._id === widgetId){
+             return widgets[w];
+            }
+        }
+    }
+
 function deleteWidget(req, res) {
     var widgetId = req.params['widgetId'];
     for(var w in widgets) {
