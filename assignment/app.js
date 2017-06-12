e05919286a6e7d48043ceb17916b71a51a6aed06
dharshini18 <govindarajan.d@husky.neu.edu>
2017-06-12 15:05:51 -0400

    Commit5 - Html widget

--- assignment/app.js
+++ assignment/app.js
@@ -1,6 +1,6 @@
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/webdev-summer1-2017');
-// mongoose.Promise = require('q').Promise;
+/*var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/webdev-summer1-2017');
+mongoose.Promise = require('q').Promise;*/
 
 console.log('server side app');
 require('./services/user.service.server');
